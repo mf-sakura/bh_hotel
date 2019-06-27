4e785b87638b7be1426839461acad111d26342fa
mf-sakura <sakura.yuto@moneyforward.co.jp>
2019-06-28 08:38:34 +0900

    fix db password

--- db/docker-entrypoint.sh
+++ db/docker-entrypoint.sh
@@ -1,9 +1,9 @@
 # Waiting mysql
-until mysqladmin ping -u root -h mysql_hotel --silent; do
+until mysqladmin ping -u root -p password -h mysql_hotel --silent; do
     echo 'waiting for mysqld to be connectable...' && sleep 3;
 done
 
 
 goose -env compose up
 
-mysql -f  -h mysql_hotel -u root bh_hotel < testdata/testdata.sql
+mysql -f  -h mysql_hotel -u root -p password bh_hotel < testdata/testdata.sql
