c719cc414b667c03b86efbd2dc49ae25e0cb4cae
mf-sakura <sakura.yuto@moneyforward.co.jp>
2019-06-28 09:15:11 +0900

    read config from environment variable

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/mf-sakura/bh_hotel/app/config"
+	"github.com/mf-sakura/bh_hotel/app/db"
 	hpb "github.com/mf-sakura/bh_hotel/app/proto"
 	"github.com/mf-sakura/bh_hotel/app/server"
 
@@ -13,6 +15,17 @@ const (
 )
 
 func main() {
+	conf, err := config.LoadConifg()
+	if err != nil {
+		panic(err)
+	}
+	dsn, err := db.CreateDataSourceName(conf.Port, conf.Host, "bh_hotel", conf.User, conf.Password)
+	if err != nil {
+		panic(err)
+	}
+	if err := db.NewDB(dsn); err != nil {
+		panic(err)
+	}
 	listen, err := net.Listen("tcp", port)
 	if err != nil {
 		panic(err)
